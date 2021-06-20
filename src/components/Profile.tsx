e0ac05ea39f0d69e44f040bf78560c4fecf1610f
Vinicius Ponce <vinicius.ponce@mazzafc.tech>
2021-06-20 17:26:54 -0300

    Update Project

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useChallenges } from '../hooks/useChallenges';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useChallenges();
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://avatars.githubusercontent.com/u/54123624?v=4" alt="vinicius ponce"/>
+      <div>
+        <strong>Vinicius Ponce</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
